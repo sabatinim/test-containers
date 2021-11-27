adc4236cdfef08bbf2649eef593c542f22a4df1c
Marco Sabatini <marco.sabatini@nozominetworks.com>
2021-11-27 11:04:56 +0100

    wait for mysql is up

--- containers/client/entrypoint.sh
+++ containers/client/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 case "$@" in
-
-schema)
-  mysql --host=mysql -uroot -ppwd < employees.sql
-  echo "EMPLOYEES schema created!"
+  schema)
+    /wait && mysql --host=mysql -uroot -ppwd < employees.sql
+    echo "EMPLOYEES schema created!"
   ;;
-demo)
-    mysql --host=mysql -uroot -ppwd < demo.sql
-  echo "Could load demo data!"
+  demo)
+    /wait && mysql --host=mysql -uroot -ppwd < demo.sql
+    echo "Could load demo data!"
+  ;;
+  *)
+    exec "$@"
   ;;
 esac
 
