1b9689c2ea4f606fe343dfca478d1379446f775d
Marco Sabatini <ms@denexus.io>
2023-02-04 01:47:03 +0100

    refactor demo app

--- containers/client/demo.sql
+++ containers/client/demo.sql
@@ -1,5 +1,3 @@
 USE birthday;
 INSERT INTO employees
-VALUES ('Marco', 'Sabatini', '05/03/1983', 'address@email.com'),
-       ('Mario', 'Russo', '05/03/1983', 'mario@email.com'),
-       ('Mario Pio', 'Gioiosa', '05/03/1983', 'mariopio@email.com');
+VALUES ('Marco', 'Sabatini', '05/03/1983', 'address@email.com');
--- src/main/kotlin/com/kata/testcontainers/App.kt
+++ src/main/kotlin/com/kata/testcontainers/App.kt
@@ -1,19 +1,26 @@
 package com.kata.testcontainers
 
 import com.kata.testcontainers.domain.Employee
+import com.kata.testcontainers.infrastructure.inMemoryLoadEmployees
+import com.kata.testcontainers.infrastructure.loadEmployeeFrom
 import com.kata.testcontainers.infrastructure.loadEmployeeWith
 
 fun main() {
-    val employees = loadEmployeeWith()
+    val dbLoadEmployees = loadEmployeeWith()
+    val fileLoadEmployees = loadEmployeeFrom("/tmp/target/test-classes/employees.txt")
+    val inMemLoadEmployees = inMemoryLoadEmployees()
 
-    println("######## Employees ########")
-    employees()
-        .map {
-            it.employeeGroup.forEach(::print)
-        }
+    println("######## DB Employees ########")
+    dbLoadEmployees().map { it.employeeGroup.forEach(::print) }
+
+    println("######## File Employees ########")
+    fileLoadEmployees().map { it.employeeGroup.forEach(::print) }
+
+    println("######## In Memory Employees ########")
+    inMemLoadEmployees().map { it.employeeGroup.forEach(::print) }
 }
 
-fun print(e:Employee){
+fun print(e: Employee) {
     println("""First Name: ${e.firstName}""")
     println("""Last Name: ${e.lastName}""")
     println("""Email: ${e.emailAddress}""")
